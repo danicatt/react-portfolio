5a5895c69ff0f19f65d9ee2073f8f83ecbe8bdb4
Danielle Catterill <daniellecatterill@MacBook-Pro.lan>
2020-08-26 22:05:44 +0100

    drafted project section layout

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react'
+import '../Projects/Projects.scss'
+import collectionProj from '../../assets/collection.png'
+import aptitudeProj from '../../assets/aptitude.png'
+import cardGame from '../../assets/armadillo-card-game.png'
+import comingSoon from '../../assets/coming-soon.jpg'
+import estatesPrj from '../../assets/estates.png'
+import finCalculator from '../../assets/finance-calculator.png'
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
 export class Projects extends Component {
     render() {
         return (
             <div>
-                
+                <Container>
+                    <Row className="row">
+                        <Col md={4}className="col1">
+                        <img src={collectionProj} alt="Cartoon Collection" className="img" />    
+                        </Col>
+                        <Col md={4}className="col2"><img src={aptitudeProj} alt="" className="img" /></Col>
+                        <Col md={4}className="col3"><img src={cardGame} alt="" className="img" /></Col>
+                        <Col md={4}className="col1"><img src={comingSoon} alt="" className="img" /></Col>
+                        <Col md={4}className="col2"><img src={estatesPrj} alt="" className="img" /></Col>
+                        <Col md={4}className="col3"><img src={finCalculator} alt="" className="img" /></Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
-}
+};
 
 export default Projects
+
+
