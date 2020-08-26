cce58f9d58b03caaabc4c04575cb266f99e58ba8
Danielle Catterill <daniellecatterill@MacBook-Pro.lan>
2020-08-26 18:44:57 +0100

    created draft about me section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
+import '../AboutMe/AboutMe.scss'
 
 export class AboutMe extends Component {
     render() {
         return (
-            <div>
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut vestibulum lectus, nec molestie dolor. Nulla sollicitudin non ligula nec luctus. Donec scelerisque erat magna, eget viverra leo condimentum gravida. Sed felis elit, pulvinar ut laoreet sed, maximus sed massa. Pellentesque ut neque felis. Nullam fringilla sem lacus, sed ullamcorper metus suscipit id. Pellentesque vehicula cursus leo, vitae suscipit metus maximus in. Donec rutrum, libero id molestie mollis, risus massa facilisis mi, quis viverra sem orci vitae arcu.
+            <section className="section1">
+                <h1>About me...</h1>
+                <h3>Being from Cornwall, I'm naturally an outdoorsy kinda person<br></br>
+                When I'm not on my laptop, I enjoy cold-water swimming, kayaking, walking <s>(to the pub)</s> with my dog or just getting out and exploring somewhere new.
+                Online, I have a strong interest in Javascript and JS frameworks, particularly ReactJS.
+                The best way to learn is by doing, so I can also be found building mini projects and expanding my knowledge of a particular subject.
+                </h3>
 
-In quis metus erat. Proin volutpat ante arcu, sed semper est finibus sed. Vestibulum erat eros, finibus non tellus nec, facilisis tincidunt sapien. Curabitur hendrerit placerat dui ac luctus. Etiam eget ullamcorper sapien. Suspendisse potenti. Etiam mattis ornare massa dapibus fermentum. Suspendisse commodo nisl arcu, et gravida tortor commodo et. Curabitur convallis ex vel nulla pellentesque, vel lacinia enim scelerisque. Aliquam in vehicula justo, non euismod dui. Ut pulvinar eleifend velit, et tempor erat iaculis non. Suspendisse ultrices diam at pharetra euismod. Quisque sit amet libero in risus dignissim elementum. Morbi velit sapien, fringilla id rhoncus eget, fringilla nec purus. Mauris faucibus sapien eros, lobortis pellentesque felis vestibulum eget. 
-                </p>
-                
-            </div>
+            </section>
+
+            
         )
     }
 }
