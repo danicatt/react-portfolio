76f59ca6b2e2d355c9c3ca61dfaf7b29ee67fc03
Danielle Catterill <daniellecatterill@MacBook-Pro.lan>
2020-08-31 18:52:22 +0100

    add css transition effect on project tiles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
 import './App.scss';
-import MainHeader from '../src/components/MainHeader/MainHeader'
-import AboutMe from '../src/components/AboutMe/AboutMe'
-import Projects from '../src/components/Projects/Projects'
+import MainHeader from '../src/components/MainHeader/MainHeader';
+import AboutMe from '../src/components/AboutMe/AboutMe';
+import Projects from '../src/components/Projects/Projects';
+
 
 export class App extends Component {
   render() {
     return (
-        <div className="App">
-          <MainHeader />
-          <AboutMe />
-          <Projects />
-        </div>
+      <div className="App">
+        <MainHeader />
+        <AboutMe />
+        <Projects />
+      </div>
     );
   }
 }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -17,12 +17,65 @@ export class Projects extends Component {
                 <Container className="section2">
                 <h1>Projects</h1>
                     <Row className="row">
-                        <Col md={4}className="col1"><img src={collectionProj} alt="Cartoon Collection" className="img" /></Col>
-                        <Col md={4}className="col2"><img src={aptitudeProj} alt="" className="img" /></Col>
-                        <Col md={4}className="col3"><img src={cardGame} alt="" className="img" /></Col>
-                        <Col md={4}className="col1"><img src={comingSoon} alt="" className="img" /></Col>
-                        <Col md={4}className="col2"><img src={estatesPrj} alt="" className="img" /></Col>
-                        <Col md={4}className="col3"><img src={finCalculator} alt="" className="img" /></Col>
+                        <Col className="col1" md={4}>
+                            <img src={collectionProj} alt="Collection App" className="img"/>
+                            <div className="middle">
+                                <div className="text">
+                                    <h3>Cartoon Collection App.</h3>
+                                    <text>An individual project written in PHP and SQL.</text>
+                                </div>
+                                <a href="https://github.com/danicatt/collector-app" className="btn btn-info">Github</a>
+                            </div>
+                        </Col>
+                        <Col className="col2" md={4}>
+                            <img src={aptitudeProj} alt="Aptitude test" className="img"/>
+                            <div class="middle">
+                                <div class="text">
+                                    <h3>Academy Aptitude Test.</h3> 
+                                    <text>Added features to an existing codebase. Compiled using Gulp and written in Javascript.</text>
+                                </div>
+                                <a href="https://github.com/Mayden-Academy/aptitude-test" className="btn btn-info">Github</a>
+                            </div>
+                        </Col>
+                        <Col className="col3" md={4}>
+                            <img src={cardGame} alt="Brain-training game" className="img"/>
+                            <div class="middle">
+                                <div class="text">
+                                    <h3>Armadillo's Brain Training Numbers Game.</h3> 
+                                    <text>Written in Javascript and a nod to Flexbox Froggy.</text>
+                                </div>
+                                <a href="http://dev.maydenacademy.co.uk/projects/2020Feb/2020-feb-numberGame/" className="btn btn-info">Demo</a>
+                            </div>
+                        </Col>
+                        <Col className="col1" md={4}>
+                            <img src={comingSoon} alt="Coming soon" className="img"/>
+                            <div className="middle">
+                                <div className="text">
+                                    <h3>To Do List.</h3> 
+                                    <text>An individual project using Slim (PHP MVC Framework) & MySQl database.</text>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col className="col2" md={4}>
+                            <img src={estatesPrj} alt="Property Website" className="img"/>
+                            <div class="middle">
+                                <div class="text">
+                                    <h3>Armadillo Estates</h3>
+                                    <text>Created using OOP and written in Javascript. With a styling nod to PHP.net!</text>
+                                </div>
+                                <a href="https://github.com/Mayden-Academy/2020-feb-propertyListing" className="btn btn-info">Github</a>
+                            </div>
+                        </Col>
+                        <Col className="col3" md={4}>
+                            <img src={finCalculator} alt="Finance Calculator" className="img"/>
+                            <div class="middle">
+                                <div class="text">
+                                    <h3>Mayden Academy Finance Calculator.</h3> 
+                                    <text>An individual project written in Javascript.</text>
+                                </div>
+                                <a href="https://github.com/danicatt/finance-calculator" className="btn btn-info">Github</a>
+                            </div>
+                        </Col>
                     </Row>
                 </Container>
             </div>
@@ -31,5 +84,3 @@ export class Projects extends Component {
 };
 
 export default Projects
-
-
