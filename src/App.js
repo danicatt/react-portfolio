56a6c6072a201e8895ed6e174af056fdeca17d58
Danielle Catterill <daniellecatterill@MacBook-Pro.lan>
2020-08-31 16:32:08 +0100

    updated socialIcons tihe FontAwesome React, adjusted padding on header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; 
 import './App.scss';
 import MainHeader from '../src/components/MainHeader/MainHeader'
 import AboutMe from '../src/components/AboutMe/AboutMe'
@@ -7,11 +7,11 @@ import Projects from '../src/components/Projects/Projects'
 export class App extends Component {
   render() {
     return (
-      <div className="App">
-        <MainHeader />
-        <AboutMe />
-        <Projects />
-      </div>
+        <div className="App">
+          <MainHeader />
+          <AboutMe />
+          <Projects />
+        </div>
     );
   }
 }
--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,16 +1,36 @@
-import React, { Component } from 'react'
-import '../MainHeader/MainHeader.scss'
+import React, { Component } from 'react';
+import '../MainHeader/MainHeader.scss';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import profilePic from '../../assets/profilePic.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export class MainHeader extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            "name":"Danielle Catterill"
+        }
+    }
+
     render() {
         return (
             <Jumbotron className="header">
-                <h1>Hey, I'm Danielle Catterill</h1>
+                <h1>Hey, I'm {this.state.name}</h1>
                 <h3>Thanks for stopping by.</h3>
                 <img src={profilePic} alt="profilePicture" className="headerImage"/>
-                </Jumbotron>
+                <div className="socialContainer">
+                    <a href="https://github.com/danicatt">
+                        <FontAwesomeIcon icon={ faGithubSquare } size='8x' style={{padding: 10}} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/danielle-catterill-013b9b75/">
+                        <FontAwesomeIcon icon={ faLinkedin } size='8x' style={{padding: 10}} />
+                    </a>
+                    
+                </div>
+            </Jumbotron>
         )
     }
 }
