9407f4ebdb04f7eb7aac9608d852932d5fdbff5d
Danielle Catterill <daniellecatterill@MacBook-Pro.lan>
2020-09-02 17:14:46 +0100

    Updates

---
+++ precache-manifest.14f0139063153580af1ea6881945255c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab0587782696abd4fa437f999e0ec44",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d60be4ee2a59e0d2c8df",
+    "url": "/react-portfolio/static/css/main.6455f873.chunk.css"
+  },
+  {
+    "revision": "51836fbf3212e72145b0",
+    "url": "/react-portfolio/static/js/2.9bfef378.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-portfolio/static/js/2.9bfef378.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d60be4ee2a59e0d2c8df",
+    "url": "/react-portfolio/static/js/main.44ae8166.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "b9b61eb9314e98162d76c0ba8fc504cb",
+    "url": "/react-portfolio/static/media/aptitude.b9b61eb9.png"
+  },
+  {
+    "revision": "9d73588c508d87e22f793211155644ea",
+    "url": "/react-portfolio/static/media/armadillo-card-game.9d73588c.png"
+  },
+  {
+    "revision": "ba54473807f166525324c52bce431b73",
+    "url": "/react-portfolio/static/media/collection.ba544738.png"
+  },
+  {
+    "revision": "381653d3ae878edece332d9e039937b8",
+    "url": "/react-portfolio/static/media/coming-soon.381653d3.jpg"
+  },
+  {
+    "revision": "83bbd2996203bdafa234499def13143c",
+    "url": "/react-portfolio/static/media/drawing.83bbd299.svg"
+  },
+  {
+    "revision": "22053fe69b5b644455e89596df682954",
+    "url": "/react-portfolio/static/media/estates.22053fe6.png"
+  },
+  {
+    "revision": "38620de21c89894d4eb83097158ae592",
+    "url": "/react-portfolio/static/media/finance-calculator.38620de2.png"
+  },
+  {
+    "revision": "1073bd0fbc4ccac4add32bf603a4cd44",
+    "url": "/react-portfolio/static/media/profilePic.1073bd0f.jpg"
+  }
+]);
