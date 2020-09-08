484536e1312558243e18dca329c8ea1a3fd7b3fd
Danielle Catterill <daniellecatterill@MacBook-Pro.lan>
2020-09-08 11:45:22 +0100

    add extra details to about me section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,8 +6,13 @@ export class AboutMe extends Component {
         return (
             <section className="section1">
                 <h1>About me...</h1>
-                <h3>Being from Cornwall, I'm naturally an outdoorsy kinda person<br></br>
-                    When I'm not on my laptop, I enjoy cold-water swimming, kayaking, walking <s>(to the pub)</s> with my dog or just getting out and exploring somewhere new.
+                <h3>
+                    I have recently graduated from Mayden Academy in Bath. Having completed their Full-Stack Track intensive course. <br/>
+                    <br/>As well as learning the fundamentals of programming, I worked within an agile team of eight students, taking on projects in JavaScript, React, Node js, PHP and Object-Orientated Programming. During this time, I also became a Certified ScrumMaster.
+                    <br/>
+                    <br/>
+                    Being from Cornwall, I'm naturally an outdoorsy kinda person.<br/>
+                    When I'm not on my laptop, I enjoy sea swimming, kayaking, walking <s>to the pub</s> with my dog or just getting out and exploring somewhere new.
                     Online, I have a strong interest in Javascript and JS frameworks, particularly ReactJS.
                     The best way to learn is by doing, so I can also be found building mini projects and expanding my knowledge of a particular subject.
                 </h3>
